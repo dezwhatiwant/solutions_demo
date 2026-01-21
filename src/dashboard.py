213dc8f177513c29fa90063549852e76cf53c8ed
Jacob Ledesma <35738182+dezwhatiwant@users.noreply.github.com>
2026-01-20 21:22:26 -0600

    Update dashboard.py

--- src/dashboard.py
+++ src/dashboard.py
@@ -5,6 +5,5 @@ def supplier_dashboard(df):
         x="Suppliers", y="Risk Score", kind="bar"
     )
     plt.title("Supplier Risk Scores")
-    plt.ylim(0.2, 0.9)
     plt.tight_layout()
     plt.show()
