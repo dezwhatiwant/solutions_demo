a40d7a2786e39c0f152a37d0e45d0329591a8856
Jacob Ledesma <35738182+dezwhatiwant@users.noreply.github.com>
2026-01-20 21:20:19 -0600

    Update dashboard.py

--- src/dashboard.py
+++ src/dashboard.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 
 def supplier_dashboard(df):
     df.sort_values("risk_score").plot(
-        x="supplier_clean", y="risk_score", kind="bar"
+        x="Suppliers", y="Risk Score", kind="bar"
     )
     plt.title("Supplier Risk Scores")
+    plt.ylim(0.2, 0.9)
     plt.tight_layout()
     plt.show()
